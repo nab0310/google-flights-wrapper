1ba3669962d15805af2004300bfedd33b2e3f67a
Nick Behrens <behrensn@iastate.edu>
2016-11-07 01:14:15 -0600

    Added Support for connecting flights

--- index.js
+++ index.js
@@ -15,28 +15,63 @@ module.exports = function(apikey){
       request({method: "post",  url: endPoint,  body: data,  json: true}, function(err, resp, body){
 
         var info = [];
+        var connectionFlights = [];
         var jsonObject = {};
-        var airline;
-        var price;
-        var arrivalTime;
-        var departureTime;
-        var flightNumber;
+        var airline = "";
+        var price = "";
+        var arrivalTime = "";
+        var departureTime = "";
+        var flightNumber = "";
+        var origin = "";
+        var destination = "";
+        var connectionNumber=0;
 
         if(body.error) return console.error(body.error);
         for(i=0; i < body.trips.tripOption.length; i++){
-          for(j=0;j<body.trips.data.carrier.length;j++){
-            if(body.trips.data.carrier[j].code==body.trips.tripOption[i].slice[0].segment[0].flight.carrier){
-                airline = body.trips.data.carrier[j].name;
-                name = body.trips.data.carrier[j].name;
-                break;
+            if(body.trips.tripOption[i].slice[0].segment.length>1){
+                for(k=0;k<body.trips.tripOption[i].slice[0].segment.length;k++){
+                    for(j=0;j<body.trips.data.carrier.length;j++){
+                        if(body.trips.data.carrier[j].code==body.trips.tripOption[i].slice[0].segment[k].flight.carrier){
+                            airline = body.trips.data.carrier[j].name;
+                            break;
+                        }
+                    }
+                    origin = body.trips.tripOption[i].slice[0].segment[k].leg[0].origin;
+                    destination = body.trips.tripOption[i].slice[0].segment[k].leg[0].destination;
+                    flightNumber = body.trips.tripOption[i].slice[0].segment[k].flight.number;
+                    price = body.trips.tripOption[i].saleTotal;
+                    arrivalTime = body.trips.tripOption[i].slice[0].segment[k].leg[body.trips.tripOption[i].slice[0].segment[k].leg.length-1].arrivalTime;
+                    departureTime = body.trips.tripOption[i].slice[0].segment[k].leg[0].departureTime;
+                    connectionNumber = connectionNumber +1;
+                    var jsonObjectConnection = {"connectionNumber: ": connectionNumber, "airline": airline , "price": price, "departureTime:":departureTime, "arrivalTime:":arrivalTime, "flightNumber:":flightNumber,"Destination: ":destination, "Origin: ": origin};
+                    connectionFlights.push(jsonObjectConnection);
+                  }
+                info.push(connectionFlights);
+                connectionFlights = [];
+            }else{
+                  for(j=0;j<body.trips.data.carrier.length;j++){
+                    if(body.trips.data.carrier[j].code==body.trips.tripOption[i].slice[0].segment[0].flight.carrier){
+                        airline = body.trips.data.carrier[j].name;
+                        break;
+                    }
+                  }
+                origin = body.trips.tripOption[i].slice[0].segment[0].leg[0].origin;
+                destination = body.trips.tripOption[i].slice[0].segment[0].leg[0].destination;
+                flightNumber = body.trips.tripOption[i].slice[0].segment[0].flight.number;
+                price = body.trips.tripOption[i].saleTotal;
+                arrivalTime = body.trips.tripOption[i].slice[0].segment[0].leg[body.trips.tripOption[i].slice[0].segment[0].leg.length-1].arrivalTime;
+                departureTime = body.trips.tripOption[i].slice[0].segment[0].leg[0].departureTime;
+              jsonObject = {"airline": airline , "price": price, "departureTime:":departureTime, "arrivalTime:":arrivalTime, "flightNumber:":flightNumber, "Destination: ":destination, "Origin: ": origin};
+                info.push(jsonObject);
             }
-          }
-          flightNumber = body.trips.tripOption[i].slice[0].segment[0].flight.number;
-          price = body.trips.tripOption[i].saleTotal;
-          arrivalTime = body.trips.tripOption[i].slice[0].segment[0].leg[body.trips.tripOption[i].slice[0].segment[0].leg.length-1].arrivalTime;
-          departureTime = body.trips.tripOption[i].slice[0].segment[0].leg[0].departureTime;
-          jsonObject = {"airline": airline , "price": price, "name:": name, "departureTime:":departureTime, "arrivalTime:":arrivalTime, "flightNumber:":flightNumber};
-          info.push(jsonObject);
+            connectionNumber =0;
+            airline = "";
+            price = "";
+            arrivalTime = "";
+            departureTime = "";
+            flightNumber = "";
+            origin = "";
+            destination = "";
         }
         fn(info);
       });
